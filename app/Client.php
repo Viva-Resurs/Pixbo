c0e3cadbfd53d901604d492507a57dc4ee005494
Christoffer Isenberg <christoffer.isenberg@gmail.com>
2015-07-16 15:20:06 +0200

    Added client

---
+++ app/Client.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Client extends Model {
+
+	/**
+	 * Table name of model.
+	 * @var string
+	 */
+	protected $table = 'clients';
+
+}
