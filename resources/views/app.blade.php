502e503481b80e2b01be25bc2d39cd826cbf537a
Xergon85 <christoffer.isenberg@gmail.com>
2015-07-26 18:19:43 +0200

    env

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,8 +6,8 @@
         @yield('title')
     </title>
     <!-- Bootstrap files -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" type="text/css" href="/css/app.css">
+
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 
