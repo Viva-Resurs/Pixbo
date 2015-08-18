61ba1f98e25954330aa6f7c82c89cf464a373814
Christoffer Isenberg <christoffer.isenberg@gmail.com>
2015-08-18 15:37:17 +0200

    Updated Image and ImageController to remove the image file at delete()

--- app/Http/Controllers/Admin/ImagesController.php
+++ app/Http/Controllers/Admin/ImagesController.php
@@ -7,99 +7,98 @@ use App\Image;
 use Illuminate\Http\Request as Requests;
 use Request;
 
-class ImagesController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     *
-     * @return Response
-     */
-    public function index()
-    {
-        $images = Image::all();
+class ImagesController extends Controller {
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index() {
+		$images = Image::all();
 
-        if (Request::wantsJson()) {
-            return $images;
-        } else {
-            $data = Image::paginate(10);
-            return view('images.index')->with('data', $data);
-        }
-    }
+		if (Request::wantsJson()) {
+			return $images;
+		} else {
+			$data = Image::paginate(10);
+			return view('images.index')->with('data', $data);
+		}
+	}
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return Response
-     */
-    public function create()
-    {
-        //
-    }
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function create() {
+		//
+	}
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  Request  $request
-     * @return Response
-     */
-    public function store(Requests $request)
-    {
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param  Request  $request
+	 * @return Response
+	 */
+	public function store(Requests $request) {
 
-        $this->validate($request, [
-            'image' => 'required|mimes:jpeg,jpg,png,bmp',
-        ]);
+		$this->validate($request, [
+			'image' => 'required|mimes:jpeg,jpg,png,bmp',
+		]);
 
-        $file = $request->file('image');
+		$file = $request->file('image');
 
-        $name = time() . $file->getClientOriginalName();
+		$name = time() . $file->getClientOriginalName();
 
-        $file->move('screens/images', $name);
+		$file->move('screens/images', $name);
 
-        Image::create(['path' => "/screens/images/{$name}", 'archived' => 0]);
-    }
+		Image::create(['path' => "/screens/images/{$name}", 'archived' => 0]);
+	}
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function show($id)
-    {
-        //
-    }
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  Image  $image
+	 * @return Response
+	 */
+	public function show(Image $image) {
+		//
+	}
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function edit($id)
-    {
-        //
-    }
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  Image  $image
+	 * @return Response
+	 */
+	public function edit(Image $image) {
+		//
+	}
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  Request  $request
-     * @param  int  $id
-     * @return Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  Request  $request
+	 * @param  Image  $image
+	 * @return Response
+	 */
+	public function update(Request $request, Image $image) {
+		//
+	}
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  Image  $image
+	 * @return Response
+	 */
+	public function destroy(Image $image) {
+		flash()->success('Image removed successfully.');
+		$deleted = $image->delete();
+
+		if (Request::wantsJson()) {
+			return (string) $deleted;
+		} else {
+			return redirect('images');
+		}
+	}
 }
--- app/Image.php
+++ app/Image.php
@@ -4,43 +4,56 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Image extends Model
-{
-    /**
-     * Table name of model.
-     *
-     * @var string
-     */
-    protected $table = 'images';
+class Image extends Model {
+	/**
+	 * Table name of model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'images';
 
-    /**
-     * [$fillable variables allowed for massallocation]
-     *
-     * @var [String]
-     */
-    protected $fillable = [
-        'path',
-        'archived',
-    ];
-    protected $appends = array('name');
+	/**
+	 * [$fillable variables allowed for massallocation]
+	 *
+	 * @var [String]
+	 */
+	protected $fillable = [
+		'path',
+		'archived',
+	];
+	protected $appends = array('name');
 
 /**
  * Get the screens associated with the given image.
  *
  * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
  */
-    public function screens()
-    {
-        $this->belongsToMany('App\Screen')->withTimestamps();
-    }
+	public function screens() {
+		$this->belongsToMany('App\Screen')->withTimestamps();
+	}
 
 /**
  * Return the attribute accessor name.
  *
  * @return String
  */
-    public function getNameAttribute()
-    {
-        return basename($this->attributes['path']);
-    }
+	public function getNameAttribute() {
+		return basename($this->attributes['path']);
+	}
+
+/**
+ * Remove the image and it's associated file.
+ *
+ * @return bool|null
+ * @throws \Exception
+ */
+	public function delete() {
+		$path = 'public' . $this->attributes['path'];
+
+		if (\File::exists($path)) {
+			\File::delete($path);
+		}
+
+		parent::delete();
+	}
 }
