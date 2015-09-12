53af576b781225f4b6ce4e85c79952220ef8df61
Christoffer Isenberg <christoffer.isenberg@gmail.com>
2015-09-12 17:05:26 +0200

    fixed a bug where you couldnt save a screengroup with the same name it had due to unique validation failing.

--- app/Http/Requests/ScreenGroupRequest.php
+++ app/Http/Requests/ScreenGroupRequest.php
@@ -4,26 +4,30 @@ namespace App\Http\Requests;
 
 use App\Http\Requests\Request;
 
-class ScreenGroupRequest extends Request {
-	/**
-	 * Determine if the user is authorized to make this request.
-	 *
-	 * @return bool
-	 */
-	public function authorize() {
-		return true;
-	}
+class ScreenGroupRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
 
-	/**
-	 * Get the validation rules that apply to the request.
-	 *
-	 * @return array
-	 */
-	public function rules() {
-		return [
-			'name' => 'required|unique:screengroups',
-			'desc' => 'required',
-			'rss_feed',
-		];
-	}
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        //dd($this->screengroups->id);
+        return [
+            'name' => 'required|unique:screengroups,name,' . $this->screengroups->id,
+            'desc' => 'required',
+            'rss_feed',
+        ];
+    }
 }
