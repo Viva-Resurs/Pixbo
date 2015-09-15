c7cb1634c7f8527e295bd0371c5abfc2a3adcc9c
Christoffer Isenberg <christoffer.isenberg@gmail.com>
2015-09-15 15:39:39 +0200

    started with scopes for Events.

--- app/Event.php
+++ app/Event.php
@@ -5,69 +5,68 @@ namespace App;
 use App\EventMeta;
 use Illuminate\Database\Eloquent\Model;
 
-class Event extends Model
-{
-    /**
-     * [$table description]
-     * @var string
-     */
-    protected $table = "events";
+class Event extends Model {
+	/**
+	 * [$table description]
+	 * @var string
+	 */
+	protected $table = "events";
 
 /**
  * [$fillable description]
  * @var array
  */
-    protected $fillable = [
-        'date',
-        'start_time',
-        'end_time',
-        'eventmeta_id',
-        'eventable_id',
-        'eventable_type',
-        'recurring',
-    ];
+	protected $fillable = [
+		'date',
+		'start_time',
+		'end_time',
+		'eventmeta_id',
+		'eventable_id',
+		'eventable_type',
+		'recurring',
+	];
 
 /**
  * Morph relation
  *
  * @return \Illuminate\Database\Eloquent\Relations\MorphTo
  */
-    public function eventable()
-    {
-        return $this->morphTo()->withTimeStamps();
-    }
+	public function eventable() {
+		return $this->morphTo()->withTimeStamps();
+	}
 
 /**
  * MetaEvent association
  *
  * @return \Illuminate\Database\Eloquent\Relations\HasOne
  */
-    public function meta()
-    {
-        return $this->hasOne('App\EventMeta');
-    }
+	public function meta() {
+		return $this->hasOne('App\EventMeta');
+	}
 
 /**
  * Get the EventMeta object
  * @return EventMeta
  */
-    public function getEventMeta()
-    {
-        return EventMeta::where(
-            'event_id', $this->getAttribute('id')
-        )->get()->first();
-    }
+	public function getEventMeta() {
+		return EventMeta::where(
+			'event_id', $this->getAttribute('id')
+		)->get()->first();
+	}
 
-    public function createAndReturnMeta()
-    {
-        $event_meta = new EventMeta;
-        $event_meta->fill([
-            'frequency'     => null,
-            'recur_type'    => null,
-            'recur_day_num' => null,
-            'recur_day'     => null,
-            'recur_end'     => null,
-        ]);
-        $this->meta()->save($event_meta);
-    }
+	public function createAndReturnMeta() {
+		$event_meta = new EventMeta;
+		$event_meta->fill([
+			'frequency' => null,
+			'recur_type' => null,
+			'recur_day_num' => null,
+			'recur_day' => null,
+			'recur_end' => null,
+		]);
+		$this->meta()->save($event_meta);
+	}
+
+	public function scopeGetAllEventsInInterval($begin, $end) {
+
+	}
 }
