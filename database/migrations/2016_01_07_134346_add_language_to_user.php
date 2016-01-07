d97346b5caf1336759cae15e8fd0666e25cda0b9
Christoffer Isenberg <christoffer.isenberg@gmail.com>
2016-01-07 13:51:34 +0100

    added lang column to user

---
+++ database/migrations/2016_01_07_134346_add_language_to_user.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class AddLanguageToUser extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function ($table) {
+            $table->string('lang')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function ($table) {
+            $table->dropColumn('lang');
+        });
+    }
+}
