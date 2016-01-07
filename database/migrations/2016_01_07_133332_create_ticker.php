712427bcf9704de8931c8b176e7686a8c79a3c66
Christoffer Isenberg <christoffer.isenberg@gmail.com>
2016-01-07 13:39:06 +0100

    updated ticker

---
+++ database/migrations/2016_01_07_133332_create_ticker.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateTicker extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('text');
+
+            $table->timestamps();
+        });
+
+        Schema::create('screen_group_ticker', function (Blueprint $table) {
+
+            $table->integer('screen_group_id')->unsigned()->index();
+            $table->foreign('screen_group_id')->references('id')->on('screengroups')->onDelete('cascade');
+            $table->integer('ticker_id')->unsigned()->index();
+            $table->foreign('ticker_id')->references('id')->on('tickers')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('tickers');
+        Schema::drop('screen_group_ticker');
+    }
+}
