b1053042edfb3087fec314e54fcc361de69888e2
Christoffer Isenberg <christoffer.isenberg@gmail.com>
2016-01-28 22:31:01 +0100

    stuff

---
+++ resources/views/users/__form.blade.php
@@ -0,0 +1,26 @@
+<div class="form-group">
+    <label for="name">{{ trans('messages.name') }}:</label>
+    {!! Form::text('name', null, ['class' => 'form-control', 'required' => 'required']) !!}
+    <small class="text-danger">{{ $errors->first('name') }}</small>
+</div>
+
+<div class="form-group">
+    {!! Form::label('password', trans('auth.password')) !!}
+    {!! Form::password('password', ['class' => 'form-control', 'required' => 'required']) !!}
+    <small class="text-danger">{{ $errors->first('password') }}</small>
+</div>
+<div class="form-group">
+    {!! Form::label('password2', trans('auth.repeat_password')) !!}
+    {!! Form::password('password2', ['class' => 'form-control', 'required' => 'required']) !!}
+    <small class="text-danger">{{ $errors->first('password') }}</small>
+</div>
+
+<div class="form-group">
+    {!! Form::label('role_id', trans('messages.role')) !!}
+    {!! Form::select('role_id', $roles, $user->role_id, ['class' => 'form-control', 'required' => 'required']) !!}
+    <small class="text-danger">{{ $errors->first('role_id') }}</small>
+</div>
+
+<div class="btn-group pull-right">
+    {!! Form::submit($submitButtonText, ['class' => 'btn btn-primary']) !!}
+</div>
