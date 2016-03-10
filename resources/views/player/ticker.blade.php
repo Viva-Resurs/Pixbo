28dfdaa5116266e8b9200e97fc3a4fc98b585ee3
ToxicTree <zoi009900@gmail.com>
2016-03-10 17:55:02 +0100

    Ticker: Fixed the JQuery magic.

--- resources/views/player/ticker.blade.php
+++ resources/views/player/ticker.blade.php
@@ -1,12 +1,10 @@
 
 @if( count($tickers) > 0)
-    <div id="controls-wrapper" style="position: absolute; top: 100%; margin-top: -10em;left:60%; margin-left: -44em;">
-        <div class="ticker-wrapperino">
-            <ul id="js-news" class="js-hidden">
-                @foreach ($tickers as $ticker)
-                    <li class="news-item">{{ $ticker->text }}</li>
-                @endforeach
-            </ul>
-        </div>
+	<div id="ticker-container">
+		<ul id="ticker">
+			@foreach ($tickers as $ticker)
+				<li>{{ $ticker->text }}</li>
+			@endforeach
+		</ul>
     </div>
 @endif
