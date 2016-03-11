2268f04ac9993fc39d01f55e59c7b9fdb70c5056
Christoffer Isenberg <christoffer.isenberg@gmail.com>
2016-03-11 13:18:50 +0100

    fixed so datatables set the language correctly with the rest of the site.

--- resources/views/shared/datagrid.blade.php
+++ resources/views/shared/datagrid.blade.php
@@ -1,9 +1,8 @@
 <script type="text/javascript">
     $(document).ready(function() {
-        var lang = $('html').attr('lang');
         $('#{{ $table_id }}').DataTable({
             "language": {
-                "url": "/js/vendor/dataTables/" + lang + ".json",
+                "url": "/js/vendor/dataTables/{{session('locale')}}.json",
             }
         });
 } );
