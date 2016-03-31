b9dfb429bdefccdf4e2159a06c1a7cbbd59a71dc
ToxicTree <zoi009900@gmail.com>
2016-03-31 16:40:54 +0200

    Clean-up for btn-group in shared.actions

--- resources/views/shared/actions.blade.php
+++ resources/views/shared/actions.blade.php
@@ -1,32 +1,26 @@
-<div class="btn-group">
-    <div class="btn-group">
-        @if(isset($modal))
-            <button type="button" class="btn btn-link" data-toggle="modal" data-target="{{ '#'.$modal.'_modal_'.$item->id }}" role="button">
-                <span class="glyphicon glyphicon-pencil" aria-hidden="true" data-toggle="tooltip" data-placement="right" title="{{ trans('messages.edit') }}"></span>
-            </button>
-        @else
-        <a href="{{ route($model.'.edit', array($item['id'])) }}" class="btn btn-link">
-            <span class="glyphicon glyphicon-pencil" aria-hidden="true" data-toggle="tooltip" data-placement="right" title="{{ trans('messages.edit') }}"></span>
+    {!! Form::open(['route' => [$model.'.destroy', $item['id']], 'method' => 'delete']) !!}
+        <button id="delete_button{{$item['id']}}" type="submit" class="btn btn-link" role="button" style="display:none;"></button>
+    {!! Form::close() !!}
+<div class="btn-group" role="group">
+    @if(isset($modal))
+        <a data-toggle="modal" data-target="{{ '#'.$modal.'_modal_'.$item->id }}" class="btn btn-link">
+            <span class="glyphicon glyphicon-pencil" aria-hidden="true" data-toggle="tooltip" data-placement="right"
+              title="{{ trans('messages.edit') }}"></span>
         </a>
-        @endif
-    </div>
-    @if(isset($from) && $from=='screengroup')
     @else
-        <div class="btn-group">
-            {!! Form::open(['route' => [$model.'.destroy', $item['id']], 'method' => 'delete']) !!}
-                <button type="submit" class="btn btn-link" role="button">
-                    <span class="glyphicon glyphicon-remove" aria-hidden="true" data-toggle="tooltip" data-placement="right" title="{{ trans('messages.remove') }}"></span>
-                </button>
-            {!! Form::close() !!}
-        </div>
+        <a href="{{ route($model.'.edit', array($item['id'])) }}" class="btn btn-link">
+            <span class="glyphicon glyphicon-pencil" aria-hidden="true" data-toggle="tooltip" data-placement="right"
+              title="{{ trans('messages.edit') }}"></span>
+        </a>
     @endif
+        <label for="delete_button{{$item['id']}}" class="btn btn-link" role="button">
+          <span class="glyphicon glyphicon-remove" aria-hidden="true" data-toggle="tooltip" data-placement="right"
+            title="{{ trans('messages.remove') }}"></span>
+        </label>
     @if(isset($preview))
-        <div class="btn-group">
-            <a href="{{ route('play.index', ['mac' => $preview, 'preview' => true]) }}" target="_blank" class="btn btn-link">
-                <span class="glyphicon glyphicon-fullscreen" aria-hidden="true" data-toggle="tooltip" data-placement="right" title="{{ trans('messages.preview') }}"></span>
-            </a>
-        </div>
+        <a href="{{ route('play.index', ['mac' => $preview, 'preview' => true]) }}" target="_blank" class="btn btn-link">
+            <span class="glyphicon glyphicon-fullscreen" aria-hidden="true" data-toggle="tooltip" data-placement="right"
+              title="{{ trans('messages.preview') }}"></span>
+        </a>
     @endif
 </div>
-
-
