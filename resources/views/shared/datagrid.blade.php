cd2260a74f0c4732778406f6121e199e63efb377
ToxicTree <zoi009900@gmail.com>
2016-04-01 15:17:34 +0200

    DataTables: Rebuilded includes making a single Loading/Include file that attach to given Tables

--- resources/views/shared/datagrid.blade.php
+++ resources/views/shared/datagrid.blade.php
@@ -1,9 +1,21 @@
-<script type="text/javascript">
-    $(document).ready(function() {
-        $('#{{ $table_id }}').DataTable({
-            "language": {
-                "url": "/js/vendor/dataTables/{{session('locale')}}.json",
-            }
-        });
-} );
+<script type="text/javascript" src="/js/vendor/dataTables/jquery.dataTables.min.js"></script>
+<link rel="stylesheet" type="text/css" href="/css/vendor/jquery.dataTables.min.css">
+<script>
+// Settings
+var datatables_settings = {
+	"language": {
+		"url": "/js/vendor/dataTables/{{session('locale')}}.json",
+	}
+};
+// Function to attach DataTable on given tables
+function datatables_attach (table_id_list_string) {
+    if (!table_id_list_string) return false;
+    var table_id_list = table_id_list_string.split(',');
+	if (!table_id_list) return false; // No IDs
+    for (var i=0 ; i<table_id_list.length ; i++){
+        $('#'+table_id_list[i]).DataTable(datatables_settings); // Attach DataTable
+    }
+}
+// Attach these tables when page is ready:
+$(document).ready(function() { datatables_attach('{{ $table_id_list_string }}'); });
 </script>
--- resources/views/tickers/index.blade.php
+++ resources/views/tickers/index.blade.php
@@ -5,19 +5,9 @@
 @stop
 
 @section('header')
-    <script type="text/javascript" src="//cdn.datatables.net/1.10.10/js/jquery.dataTables.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.10/css/jquery.dataTables.min.css">
+    @include('shared.datagrid', ['table_id_list_string' => 'tickers_table'])
 @stop
-
 
 @section('content')
-
     @include('tickers.table', ['from' => 'tickers'])
-
-@stop
-
-@section('footer')
-
-    @include('shared.datagrid', ['table_id' => 'tickers_table'])
-
 @stop
