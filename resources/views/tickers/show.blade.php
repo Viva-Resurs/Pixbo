7694c38c64249ec9909131b091dbc92b20cc0d77
Christoffer Isenberg <christoffer.isenberg@gmail.com>
2016-04-04 13:52:33 +0200

    Fixed the ticker@show

--- resources/views/tickers/show.blade.php
+++ resources/views/tickers/show.blade.php
@@ -10,12 +10,17 @@
 <h1 class="page-header">{{ trans("messages.show_ticker") }}</h1>
 
 <div class="row">
-    <div class="col-sm-6 col-md-4">
+    <div class="col-lg-12">
         <div class="panel panel-default">
-              <div class="panel-body">
-                    <schedule id="{{ $ticker->id }}" model="tickers"></schedule>
-                    @include('shared.schedule__template')
-              </div>
+            <div class="panel-body">
+                <schedule id="{{ $ticker->id }}" model="tickers"></schedule>
+                @include('shared.schedule.template', ['item' => $ticker, 'model' => 'tickers'])
+            </div>
+
+            <div class="panel-footer">
+                @include('shared.schedule.submit_button', ['item' => $ticker, 'model' => 'tickers'])
+            </div>
+
         </div>
     </div>
 </div>
