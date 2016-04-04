c281c0445fe16391f68aa68e47e69dff913afe7f
Christoffer Isenberg <christoffer.isenberg@gmail.com>
2016-04-04 15:01:19 +0200

    Fixed so screengroup@edit 'works'

--- resources/views/screengroups/edit.blade.php
+++ resources/views/screengroups/edit.blade.php
@@ -1,12 +1,7 @@
 @extends('admin')
 
-
-@section('header')
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.0.1/dropzone.css">
-@stop
-
 @section('title')
-    ScreenGroup index
+   {{ trans('messages.screen_group_index') }}
 @stop
 
 
@@ -15,37 +10,10 @@
     <h1 class="page-header">{{ trans("messages.edit_screen_group") }}</h1>
 
     <div class="row">
-        <div class="col-md-4">
-
-            @include('screengroups.edit.edit__info')
-            @include('shared.edit__event')
-
+        <div class="col-lg-12">
+            {!! Form::model($screengroup, ['method' => 'PATCH', 'route' => ['admin.screengroups.update', $screengroup->id]]) !!}
+                @include('screengroups.__form', ['submitButtonText' => trans('messages.save')])
+            {!! Form::close() !!}
         </div>
-
-        <div class="col-md-8">
-
-
-            <!-- Add ticker here -->
-            @include('screengroups.edit.edit__screens')
-
-        </div>
-
-    <!-- Modal -->
-    @include('events.meta.__form')
-
-@stop
-
-@section('footer')
-
-    <!-- Required for screen uploading -->
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.0.1/dropzone.js"></script>
-    <script>
-        Dropzone.options.addImageForm = {
-            paramName: 'photo',
-            maxFileSize: 10,
-            acceptedFiles: '.jpg,.jpeg,.png,.bmp',
-            dictDefaultMessage: 'Drop files here to be uploaded.'
-        };
-    </script>
-
+    </div>
 @stop
