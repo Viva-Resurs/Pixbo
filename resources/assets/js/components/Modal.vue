0abd05d8ca5fa1a1183bd31c1923cba0ef28a01f
Christoffer Isenberg <christoffer.isenberg@gmail.com>
2016-04-05 15:40:21 +0200

    Started work on a modal component

---
+++ resources/assets/js/components/Modal.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="modal fade" tabindex="-1" role="dialog" :id="'screens_modal_' + model.id">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title">{{ title }}</h4>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="panel panel-default">
+                                <div class="panel-body">
+                                    <slot name="body"></slot>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <slot name="footer"></slot>
+                </div>
+            </div><!-- /.modal-content -->
+        </div><!-- /.modal-dialog -->
+    </div><!-- /.modal -->
+</template>
+
+<script>
+    export default {
+        props: ['model', 'title'],
+
+        data: function() {
+            return {
+                asdF: null,
+            };
+        },
+    }
+</script>
