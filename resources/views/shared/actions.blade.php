22d8adab212547eb9155d896c744c79063d1f810
Christoffer Isenberg <christoffer.isenberg@gmail.com>
2016-04-14 13:15:47 +0200

    Fixed a bug where you only saw the remove association was if you had remove privilegies,, now you see it if you have edit privs.

--- resources/views/shared/actions.blade.php
+++ resources/views/shared/actions.blade.php
@@ -12,19 +12,22 @@
             </a>
         @endif
     @endcan
-    @can('remove_'.$from)
+
         @if($from=='screengroups' && $model!='admin.screengroups')
-            <a href="/admin/screengroups/{{ $screengroup->id }}/{{get_model_name($model)}}/{{ $item->id }}/remove_{{get_model_name($model)}}_association">
-              <span class="glyphicon glyphicon-minus" aria-hidden="true" data-toggle="tooltip" data-placement="right"
-                title="{{ trans('messages.remove_association_tooltip', ['association' => $screengroup->name]) }}"></span>
-            </a>
+            @can('edit_'.$from)
+                <a href="/admin/screengroups/{{ $screengroup->id }}/{{get_model_name($model)}}/{{ $item->id }}/remove_{{get_model_name($model)}}_association">
+                  <span class="glyphicon glyphicon-minus" aria-hidden="true" data-toggle="tooltip" data-placement="right"
+                    title="{{ trans('messages.remove_association_tooltip', ['association' => $screengroup->name]) }}"></span>
+                </a>
+            @endcan
         @else
-            <label for="delete_button{{$item['id']}}" class="btn btn-link" role="button">
-              <span class="glyphicon glyphicon-remove" aria-hidden="true" data-toggle="tooltip" data-placement="right"
-                title="{{ trans('messages.remove') }}"></span>
-            </label>
+            @can('remove_'.$from)
+                <label for="delete_button{{$item['id']}}" class="btn btn-link" role="button">
+                  <span class="glyphicon glyphicon-remove" aria-hidden="true" data-toggle="tooltip" data-placement="right"
+                    title="{{ trans('messages.remove') }}"></span>
+                </label>
+            @endcan
         @endif
-    @endcan
     @if(isset($preview))
         <a href="{{ route('play.index', ['mac' => $preview, 'preview' => true]) }}" target="_blank" class="btn btn-link">
             <span class="glyphicon glyphicon-fullscreen" aria-hidden="true" data-toggle="tooltip" data-placement="right"
