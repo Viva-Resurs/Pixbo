c96e6f373c1b4e635d61db92e34559524f44a598
ToxicTree <zoi009900@gmail.com>
2016-04-19 14:23:05 +0200

    CSS: main div

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -3,14 +3,14 @@
 @section('body')
 <div class="container-fluid">
     <div class="row">
-        <div class="col-sm-12 col-sm-offset-0 col-md-10 col-md-offset-1 main">
+        <div class="main">
 
             <!--  Flash messages -->
             <div id="alerts">
                 @include ('flash::message')
                 <toast></toast>
             </div>
-            <div class="container">
+            <div class="row">
                 @yield('content')
             </div>
         </div>
