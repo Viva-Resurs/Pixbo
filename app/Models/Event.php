d6b94028bfd21aded27c1a5e06d93af76d234c4b
Christoffer Isenberg <christoffer.isenberg@gmail.com>
2016-04-20 15:25:18 +0200

    Added Event and ShadowEvent along with it's files, also added a helpers file.

---
+++ app/Models/Event.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\HasShadowEvents;
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    use HasShadowEvents;
+    protected $table = "events";
+
+    /**
+     * [$fillable description]
+     * @var array
+     */
+    protected $fillable = [
+        'start_date',
+        'end_date',
+        'start_time',
+        'end_time',
+        'frequency',
+        'recur_type',
+        'recur_day_num',
+        'recur_day',
+        'days_before_event',
+        'eventable_id',
+        'eventable_type',
+    ];
+
+    protected $touches = ['eventable'];
+
+    public static function boot() {
+        parent::boot();
+
+        Event::created(function($event) {
+            $event->generateShadowEvents($event);
+        });
+
+        Event::deleting(function($event) {
+            $event->shadow_events()->delete();
+        });
+        Event::updated(function ($event) {
+            $event->shadow_events()->delete();
+            $event->generateShadowEvents($event);
+        });
+    }
+
+    /**
+     * Morph relation
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\MorphTo
+     */
+    public function eventable()
+    {
+        return $this->morphTo();
+    }
+
+    public function getFrequencyAttribute($value)
+    {
+        return !is_null($value) ? $value : '1';
+    }
+
+    public function getStartTimeAttribute($value)
+    {
+        return !is_null($value) ? $value : '00:00';
+    }
+
+    public function getEndTimeAttribute($value)
+    {
+        return !is_null($value) ? $value : '23:59';
+    }
+
+    public function getDaysBeforeEventAttribute($value)
+    {
+        return !is_null($value) ? $value : '0';
+    }
+
+    public function getRecurDayAttribute($value)
+    {
+        return !is_null($value) ? $value : '1';
+    }
+
+    public function getRecurDayNumAttribute($value)
+    {
+        return !is_null($value) ? $value : json_encode("1");
+    }
+}
---
+++ database/migrations/2016_04_20_130309_create_event_tables.php
@@ -0,0 +1,68 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->date('start_date');
+            $table->date('end_date')->nullable();
+
+            $table->time('start_time')->nullable();
+            $table->time('end_time')->nullable();
+
+            $table->integer('frequency')->unsigned()->nullable();
+            $table->string('recur_type')->nullable();
+            $table->string('recur_day_num')->nullable();
+            $table->string('recur_day')->nullable();
+            $table->date('recur_end')->nullable();
+            $table->integer('days_before_event')->nullable();
+
+            $table->integer('eventable_id');
+            $table->string('eventable_type');
+
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')
+                ->references('id')->on('users')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+
+        Schema::create('shadow_events', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('title');
+            $table->boolean('isAllDay');
+            $table->dateTime('start');
+            $table->dateTime('end');
+            $table->integer('event_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('event_id')
+                ->references('id')
+                ->on('events')
+                ->onDelete('cascade');;
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('events');
+        Schema::drop('shadow_events');
+    }
+}
