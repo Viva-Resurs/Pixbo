4ad384ad6989aead7f24a341ebdf6be48a4300d1
ToxicTree <zoi009900@gmail.com>
2016-04-21 16:14:41 +0200

    Auth: Added some translations & 'forgot password?' on failed login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -19,7 +19,7 @@
             </div>
 
             <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
+                <div class="panel-heading">{{ trans('auth.login') }}</div>
                 <div class="panel-body">
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/auth/login') }}">
                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
@@ -35,6 +35,9 @@
                             <label class="col-sm-4 col-md-4 col-lg-4 control-label">{{ trans('auth.password') }}</label>
                             <div class="col-sm-5 col-md-5 col-lg-5">
                                 <input type="password" class="form-control" name="password">
+                                @if (count($errors)>0)
+                                    <div class="pull-right"><a href="">{{ trans('auth.forgot') }}</a></div>
+                                @endif
                             </div>
                         </div>
 
@@ -44,7 +47,7 @@
                                     <label>
                                         <input type="checkbox" name="remember"> {{ trans('auth.remember_me') }}
                                     </label>
-                                        <button type="submit" class="btn btn-primary pull-right">Login</button>
+                                        <button type="submit" class="btn btn-primary pull-right">{{ trans('auth.login') }}</button>
                                 </div>
                             </div>
                         </div>
