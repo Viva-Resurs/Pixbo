95e9c3abe904d4b98067a47a2dd9d0d8f6039d16
ToxicTree <zoi009900@gmail.com>
2016-04-21 13:22:37 +0200

    CSS: Removed unused classes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,51 +2,49 @@
 
 @section('body')
 <div class="container-fluid">
-    <div class="row panel-auth">
-        <div class="col-md-6 col-md-offset-3">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-                <div class="panel-body">
-                    @if (count($errors) > 0)
-                        <div class="alert alert-danger">
-                            {{ trans('validation.error_input') }}<br><br>
-                            <ul>
-                                @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
+    <div class="main">
+        <div class="panel panel-default">
+            <div class="panel-heading">Login</div>
+            <div class="panel-body">
+                @if (count($errors) > 0)
+                    <div class="alert alert-danger">
+                        {{ trans('validation.error_input') }}<br><br>
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+
+                <form class="form-horizontal" role="form" method="POST" action="{{ url('/auth/login') }}">
+                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+                    <div class="form-group">
+                        <label class="col-md-4 control-label">{{ trans('auth.username') }}</label>
+                        <div class="col-md-6">
+                            <input type="text" class="form-control" name="name" value="{{ old('name') }}">
                         </div>
-                    @endif
+                    </div>
 
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/auth/login') }}">
-                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                    <div class="form-group">
+                        <label class="col-md-4 control-label">{{ trans('auth.password') }}</label>
+                        <div class="col-md-6">
+                            <input type="password" class="form-control" name="password">
+                        </div>
+                    </div>
 
-                        <div class="form-group">
-                            <label class="col-md-4 control-label">{{ trans('auth.username') }}</label>
-                            <div class="col-md-6">
-                                <input type="text" class="form-control" name="name" value="{{ old('name') }}">
+                    <div class="form-group">
+                        <div class="col-md-6 col-md-offset-4">
+                            <div class="checkbox">
+                                <label>
+                                    <input type="checkbox" name="remember"> {{ trans('auth.remember_me') }}
+                                </label>
+                                    <button type="submit" class="btn btn-primary pull-right">Login</button>
                             </div>
                         </div>
-
-                        <div class="form-group">
-                            <label class="col-md-4 control-label">{{ trans('auth.password') }}</label>
-                            <div class="col-md-6">
-                                <input type="password" class="form-control" name="password">
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember"> {{ trans('auth.remember_me') }}
-                                    </label>
-                                        <button type="submit" class="btn btn-primary pull-right">Login</button>
-                                </div>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
