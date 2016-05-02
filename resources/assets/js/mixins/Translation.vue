213d9b17b23a5b89142bf5772d1098eefe851849
ToxicTree <zoi009900@gmail.com>
2016-05-02 14:53:29 +0200

    Translation.vue will now init lang-data from json-files

--- resources/assets/js/mixins/Translation.vue
+++ resources/assets/js/mixins/Translation.vue
@@ -1,12 +1,58 @@
 <script>
     export default {
+        data: function(){
+            return {
+                lang: this.$root.$data.lang,
+                loaded: this.$root.$data.loaded,
+                modules: [
+                    'general',
+                    'ticker',
+                    'screen',
+                    'datetimepicker_tooltip',
+                ],
+            }
+        },
         methods: {
+            init: function (){
+                var self = this;
+                self.$root.$data.lang=[];
+                for (var i=0 ; i<self.modules.length ; i++){
+                    $.ajax({
+                        url: '/lang/'+self.modules[i]+'.sv.lang',
+                        indexValue: i,
+                        success: function(result){
+                            try {
+                                self.$root.$data.lang[self.modules[this.indexValue]] = JSON.parse(result);
+                            } catch (err) {
+                                console.log(err);
+                            }
+                        }
+                    });
+                }
+                self.$root.$data.loaded = true;
+            },
             trans: function (string) {
-                return string;
+                var scope = string.split('.')[0];
+                var word = string.split('.')[1];
+
+                if (this.lang && this.lang[scope] && this.lang[scope][word])
+                    return this.lang[scope][word];
+                else
+                    return string; // Fallback
             },
             trans_choice: function(string, num) {
-                return string;
+                var scope = string.split('.')[0];
+                var word = string.split('.')[1];
+                
+                if (this.lang && this.lang[scope] && this.lang[scope][word])
+                    return this.lang[scope][word].split('|')[num];
+                else
+                    return string; // Fallback
             },
+        },
+        created: function(){
+            if (!this.$root.$data.loaded)
+                this.init();
         },
     };
 </script>
