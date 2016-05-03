104086fc983315acc70fdcef8fd96ef13aef199e
Christoffer Isenberg <christoffer.isenberg@gmail.com>
2016-05-03 19:44:18 +0200

    Logout now just dispatches a "userHasLoggedOut" event, instead of clearing stuff out by it self

--- resources/assets/js/pages/auth/Logout.vue
+++ resources/assets/js/pages/auth/Logout.vue
@@ -3,10 +3,13 @@
 
         route: {
             activate: function (transition) {
+                this.$dispatch('userHasLoggedOut')
+                /*
                 this.$root.authenticated = false
                 this.$root.user = null
                 localStorage.removeItem('user')
                 localStorage.removeItem('jwt-token')
+                */
                 transition.redirect('/')
             }
         }
