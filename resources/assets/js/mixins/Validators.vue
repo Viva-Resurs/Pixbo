7375e654e879b1573cca2ff1b28521a79cb88427
Christoffer Isenberg <christoffer.isenberg@gmail.com>
2016-05-15 14:15:34 +0200

    Added Validator mixin

---
+++ resources/assets/js/mixins/Validators.vue
@@ -0,0 +1,7 @@
+<script type="text/ecmascript-6">
+    export default {
+        methods: {
+            isIPAddress: val => /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|$)){4}$/.test(val)
+        }
+    }
+</script>
