0e020da0fa9b08f04242aef1e8cac5f7ab5c4bd9
ToxicTree <zoi009900@gmail.com>
2016-05-16 13:56:35 +0200

    Updated description of exceptions-lang

--- resources/lang/en/exceptions.php
+++ resources/lang/en/exceptions.php
@@ -4,12 +4,10 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Authentication Language Lines
+    | Exceptions Language Lines
     |--------------------------------------------------------------------------
     |
-    | The following language lines are used during authentication for various
-    | messages that we need to display to the user. You are free to modify
-    | these language lines according to your application's requirements.
+    | The following language lines are used for exception-messages
     |
     */
     
