13093128eca6f78d40142fa58a8675d202ef9c44
Christoffer Isenberg <christoffer.isenberg@gmail.com>
2016-05-25 14:47:31 +0200

    Cleaned up the Screens@show some

--- resources/assets/js/pages/screens/show.vue
+++ resources/assets/js/pages/screens/show.vue
@@ -8,24 +8,20 @@
 
     <div v-if=" ! $loadingRouteData">
         <div class="panel-body">
-           <schedule :model.sync="screen"></schedule>
+            <schedule :model.sync="screen"></schedule>
         </div>
     </div>
 </template>
 
 <script>
-    module.exports = {
+    import Schedule from '../../components/Schedule.vue'
+    export default {
 
-        // TODO: Clean this up abit...
-
-        components: {
-            Schedule: require('./../../components/Schedule.vue')
-        },
+        components: { Schedule },
 
         data: function () {
             return {
-                screen: {},
-                screengroups: []
+                screen: {}
             }
         },
 
@@ -33,42 +29,17 @@
             fetch: function (id, successHandler) {
                 var that = this
                 client({ path: '/screens/' + id }).then(
-                        function (response) {
-                            that.$set('screen', response.entity.data)
-                            successHandler(response.entity.data)
-                        },
-                        function (response, status, request) {
-                            if (status === 401) {
-                                self.$dispatch('userHasLoggedOut')
-                            } else {
-                                //console.log(response)
-                            }
-                        }
-                )
-            },
-
-
-            updateScreen: function (e) {
-                e.preventDefault()
-                var self = this
-                client({ path: '/screens/' + this.screen.id, entity: this.screen, method: 'PUT'}).then(
                     function (response) {
-
-                        self.$dispatch('alert', {
-                            message: self.trans('screen.updated'),
-                            options: {theme: 'success'}
-                        })
-
+                        that.$set('screen', response.entity.data)
+                        successHandler(response.entity.data)
                     },
-                    function (response) {
-                        self.$dispatch('alert', {
-                            message: self.trans('screen.updated_fail'),
-                            options: {theme: 'error'}
-                        })
+                    function (response, status, request) {
+                        if (status === 401) {
+                            self.$dispatch('userHasLoggedOut')
+                        }
                     }
                 )
             }
-
         },
 
         route: {
