f9fb03f375643d5ef6ae01ed653a7fee7fa69312
Christoffer Isenberg <christoffer.isenberg@gmail.com>
2016-05-25 14:45:46 +0200

    Added an Options-Arrays file, where static arrays of options can be saved

---
+++ resources/assets/js/option_arrays.js
@@ -0,0 +1,7 @@
+export const recur_options = [
+    {id: 'none',    name:   'schedule.never'},
+    {id: 'daily',   name:   'schedule.daily'},
+    {id: 'weekly',  name:   'schedule.weekly'},
+    {id: 'monthly', name:   'schedule.monthly'},
+    {id: 'yearly',  name:   'schedule.yearly'}
+];
