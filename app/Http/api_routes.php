ad834c370e88f4d64db48cb0bace53414e861b73
Christoffer Isenberg <christoffer.isenberg@gmail.com>
2016-05-29 15:54:13 +0200

    Added temporary api endpoint called categorys, due to lack of magic to pluralize category at the client side.

--- app/Http/api_routes.php
+++ app/Http/api_routes.php
@@ -15,6 +15,8 @@ $api->version('v1', function ($api) {
     $api->group(['middleware' => ['api.auth']], function ($api) {
         $api->resource('clients', 'App\Api\V1\Controllers\ClientController');
         $api->resource('categories', 'App\Api\V1\Controllers\CategoryController');
+        // TODO: Fix Dump frontend is dump
+        $api->resource('categorys', 'App\Api\V1\Controllers\CategoryController');
         $api->resource('screengroups', 'App\Api\V1\Controllers\ScreenGroupController');
         $api->resource('screens', 'App\Api\V1\Controllers\ScreenController');
         $api->resource('tickers', 'App\Api\V1\Controllers\TickerController');
