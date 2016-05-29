4029488366e004791bd904861110a804a3c1c627
Christoffer Isenberg <christoffer.isenberg@gmail.com>
2016-05-29 18:08:22 +0200

    Updated ToS

--- resources/assets/js/pages/Terms.vue
+++ resources/assets/js/pages/Terms.vue
@@ -13,7 +13,7 @@
 <pre>
 The MIT License (MIT)
 
-Copyright (c) 2015 Yourname
+Copyright (c) 2016 Christoffer Isenberg, Jonathan Timmerlid
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
