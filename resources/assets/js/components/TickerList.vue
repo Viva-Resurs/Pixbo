60bfdfdf1c1bba3c061b933384e6d9feb0bd43b7
ToxicTree <zoi009900@gmail.com>
2016-06-27 13:56:16 +0200

    Tickers: minify screengroups name (screengroups>3) + links

--- resources/assets/js/components/TickerList.vue
+++ resources/assets/js/components/TickerList.vue
@@ -20,19 +20,26 @@
         <tr v-for="ticker in tickers">
             <td>{{ ticker.id }}</td>
             <td><a v-link="{ path: '/tickers/'+ticker.id }">{{ ticker.text }}</a></td>
-            <!-- TODO: Minify name and link? -->
-            <td><span v-for="sg in getScreengroup(ticker)">{{ sg.name }}</span></td>
+            <td>
+                <span v-for="sg in getScreengroup(ticker)">
+                    <template v-if="$route.path == '/screengroups/'+sg.id">
+                        <b>{{ sg.name }}</b>
+                    </template>
+                    <template v-else>
+                        <a v-if="ticker.screengroups.length>3" v-link="{ path: '/screengroups/'+sg.id }"
+                           v-tooltip data-original-title='{{sg.name}}'>{{ minify(sg.name) }}</a>
+                        <a v-else v-link="{ path: '/screengroups/'+sg.id }">{{ sg.name }}</a>
+                    </template>
+                    
+                </span>
+            </td>
             <td>
                 <a class="btn btn-primary btn-xs fa fa-pencil" v-link="{ path: '/tickers/'+ticker.id }"
                    v-tooltip data-original-title="{{ trans('general.edit') }}"></a>
-                <template v-if="this.from=='screengroup'">
-                    <a class="btn btn-primary btn-xs fa fa-minus" v-on:click="removeTicker($index)"
-                       v-tooltip data-original-title="{{ trans('screengroup.remove_association')  }}"></a>
-                </template>
-                <template v-else>
-                    <a class="btn btn-primary btn-xs fa fa-times" v-on:click="removeTicker($index)"
-                       v-tooltip data-original-title="{{ trans('general.delete')  }}"></a>
-                </template>
+                <a v-if="this.from=='screengroup'" class="btn btn-primary btn-xs fa fa-minus" v-on:click="removeTicker($index)"
+                   v-tooltip data-original-title="{{ trans('screengroup.remove_association') }}"></a>
+                <a v-else class="btn btn-primary btn-xs fa fa-times" v-on:click="removeTicker($index)"
+                   v-tooltip data-original-title="{{ trans('general.delete') }}"></a>
             </td>
         </tr>
         </tbody>
@@ -43,7 +50,13 @@
     export default {
         props: ['tickers','from'],
 
+        computed: {
+
+        },
         methods: {
+            minify(name){
+                return name.substring(0,7)+'..';
+            },
             removeTicker(index) {
                 this.$dispatch('remove-ticker', index)
             },
