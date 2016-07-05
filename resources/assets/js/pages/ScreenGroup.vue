605e191ea485c2b08065313e7cc784ec0e3c4c7e
ToxicTree <zoi009900@gmail.com>
2016-07-05 16:28:21 +0200

    just a /

--- resources/assets/js/pages/ScreenGroup.vue
+++ resources/assets/js/pages/ScreenGroup.vue
@@ -13,7 +13,7 @@
                     <div class="app-tabs">
                         <ul class="nav app-tabs-stacked">
                             <li>
-                                <a v-link="{ path: '/screengroups/', exact: true }">
+                                <a v-link="{ path: '/screengroups', exact: true }">
                                     <i class="fa fa-btn fa-fw fa-list"></i>&nbsp;{{ trans('general.archive') }}</span>
                                 </a>
                             </li>
