08c6b21e3a199912a5c3b50779046a8976583586
ToxicTree <zoi009900@gmail.com>
2016-08-29 13:40:30 +0200

    Test2: mail

--- resources/views/emails/password.blade.php
+++ resources/views/emails/password.blade.php
@@ -1,2 +1,15 @@
 <?php
+
+echo "About to send";
+// the message
+$msg = "First line of text\nSecond line of text";
+
+// use wordwrap() if lines are longer than 70 characters
+$msg = wordwrap($msg,70);
+
+// send email
+mail("henrik.persson@umea.se","TEST",$msg);
+
+echo "Sent";
+
 phpinfo();
