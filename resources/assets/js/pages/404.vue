87df01773a82f580e889e7b0c71838e94ac37601
ToxicTree <zoi009900@gmail.com>
2016-09-13 15:10:39 +0200

    404: Added some rotating bugs for missing routes...

--- resources/assets/js/pages/404.vue
+++ resources/assets/js/pages/404.vue
@@ -1,31 +1,75 @@
 <style>
-    .title {
-        color: #999;
-        font-weight: 100;
-        font-family: 'Lato', Helvetica, sans-serif;
-        font-size: 60px;
-        margin-bottom: 40px;
-        text-align: center;
-        margin-top: 20%;
+    .moving_bug {
+        color: #501;
+        position: relative;
+        font-size: 20px;
+        top: -50px;
     }
-    .title a {
-        display: block;
-        margin-top: 20px;
+    .bug1 {
+        top: -100px;
+        animation: key1 2s linear infinite;
     }
-    .title a:hover {
-        text-decoration: none;
+    @keyframes key1 {
+        from {
+            transform: rotate(0deg)
+                       translate(-10px)
+                       rotate(0deg);
+        }
+        to {
+            transform: rotate(360deg)
+                       translate(-10px) 
+                       rotate(-360deg);
+        }
     }
+    .bug2 {
+        left: -50px;
+        animation: key2 1s linear infinite;
+    }
+    @keyframes key2 {
+        from {
+            transform: rotate(0deg)
+                       translate(5px)
+                       rotate(0deg);
+        }
+        to {
+            transform: rotate(360deg)
+                       translate(5px) 
+                       rotate(-360deg);
+        }
+    }
+    .bug3 {
+        left: 50px;
+        animation: key3 3s linear infinite;
+    }
+    @keyframes key3 {
+        from {
+            transform: rotate(0deg)
+                       translate(15px)
+                       rotate(0deg);
+        }
+        to {
+            transform: rotate(360deg)
+                       translate(15px) 
+                       rotate(-360deg);
+        }
+    }
+
 </style>
 
 <template>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-3"></div>
-            <div class="col-md-6 title">
+
+    <div class="container app-screen text-center">
+
+
+
+        <div class="fa fa-5x fa-chain-broken"></div>
+        <br>
+    <div class="moving_bug bug1 fa fa-1x fa-bug"></div>
+    <div class="moving_bug bug2 fa fa-1x fa-bug"></div>
+    <div class="moving_bug bug3 fa fa-1x fa-bug"></div>
+        <br>
                 Sorry, we couldn't find what you were looking for :-(<br>
                 <a href="/">Go back to the homepage</a>
-            </div>
-            <div class="col-md-3"></div>
-        </div>
+
     </div>
 </template>
