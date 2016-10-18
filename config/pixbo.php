6c12a7a4bf3319b94da50ab641c7b32f311a830f
ToxicTree <zoi009900@gmail.com>
2016-10-18 10:33:21 +0200

    Details in config

--- config/pixbo.php
+++ config/pixbo.php
@@ -4,42 +4,30 @@
  * Pixbo default settings, only used when setting up the database.
  */
 
-$settings = [
-    'admin' => [
-        'username' => 'admin',
-    ],
-    'moderator' => [
-        'username' => 'moderator',
-    ],
-    'domain' => "viva.se", // config('app.domain') Server can´t get this?
-    'default_category_name' => "Ej schemalagda",
-    'default_screengroup_dummy_name' => "Dummy"
-];
-
 return [
     'settings'=> [
         'administration' => [
             'administrator' => [
-                'username' => $settings['admin']['username'],
+                'username' => 'admin',
                 // Generated from the username, do not change.
-                'password' => env('PIXBO_ADMIN_PASSWORD', $settings['admin']['username']),
-                'email' => $settings['admin']['username'].'@'.$settings['domain']
+                'password' => env('PIXBO_ADMIN_PASSWORD', 'admin'),
+                'email' => 'admin@viva.se'
             ],
             'moderator' => [
-                'username' => $settings['moderator']['username'],
+                'username' => 'moderator',
                 // Generated from the username, do not change.
-                'password' => env('PIXBO_MOD_PASSWORD', $settings['moderator']['username']),
-                'email' => $settings['moderator']['username'].'@'.$settings['domain']
+                'password' => env('PIXBO_MOD_PASSWORD', 'moderator'),
+                'email' => 'moderator@viva.se'
             ]
 
         ],
 
         'category' => [
-            'default_name' => $settings['default_category_name']
+            'default_name' => "Ej schemalagda"
         ],
 
         'screengroup' => [
-            'dummy_name' => $settings['default_screengroup_dummy_name']
+            'dummy_name' => "Dummy"
         ],
 
         'player' => [
