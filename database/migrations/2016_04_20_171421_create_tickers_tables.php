3a892666235c137e87a202e03635a1f763834f0f
ToxicTree <zoi009900@gmail.com>
2016-10-31 11:38:22 +0100

    Migration renames

--- database/migrations/2016_04_20_171421_create_tickers_tables.php
+++ database/migrations/2016_04_20_171421_create_tickers_tables.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTickersTable extends Migration
+class CreateTickersTables extends Migration
 {
     /**
      * Run the migrations.
