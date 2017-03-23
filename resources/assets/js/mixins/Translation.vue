d9edf78bdace7969511b886d6c2d4c0d9f22e4b2
ToxicTree <zoi009900@gmail.com>
2017-03-23 13:28:17 +0100

    GUI: Translation, set lang data when created

--- resources/assets/js/mixins/Translation.vue
+++ resources/assets/js/mixins/Translation.vue
@@ -18,14 +18,14 @@
 
                 var scope = string.split('.')[0];
                 var word = string.split('.')[1];
-                
+
                 if (this.lang && this.lang[scope] && this.lang[scope][word])
                     if (num!=1)
                         return (this.lang[scope][word].split('|').length>1) ? this.lang[scope][word].split('|')[1] : this.lang[scope][word];
 
                     else
                         return this.lang[scope][word].split('|')[0];
-                
+
                 else {
 
                     console.log("Add lang for: " + string);
@@ -42,6 +42,7 @@
                 this.langChoise = l;
             else
                 localStorage.setItem('lang', this.defaultLang);
+            this.lang = this.langList[l];
         },
 
         watch: {
@@ -49,7 +50,7 @@
             langChoise: function(val, oldVal){
 
                 localStorage.setItem('lang', val);
-                
+
                 this.lang = this.langList[val];
 
                 if (oldVal != false)
