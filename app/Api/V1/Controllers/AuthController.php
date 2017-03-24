2f399128d7c35b7f69b98d350b4fcd05643c62f8
ToxicTree <zoi009900@gmail.com>
2017-03-24 10:51:16 +0100

    API: Auth, signup removed (Users are created at UserController)

--- app/Api/V1/Controllers/AuthController.php
+++ app/Api/V1/Controllers/AuthController.php
@@ -43,31 +43,6 @@ class AuthController extends Controller
         return response()->json(compact('token'));
     }
 
-    public function signup(Request $request)
-    {
-        $signupFields = Config::get('boilerplate.signup_fields');
-        $hasToReleaseToken = Config::get('boilerplate.signup_token_release');
-
-        $userData = $request->only($signupFields);
-
-        $validator = Validator::make($userData, Config::get('boilerplate.signup_fields_rules'));
-
-        if($validator->fails())
-            throw new ValidationHttpException($validator->errors()->all());
-
-        User::unguard();
-        $user = User::create($userData);
-        User::reguard();
-
-        if (!$user->id)
-            return $this->response->error('could_not_create_user', 500);
-
-        if ($hasToReleaseToken)
-            return $this->login($request);
-        
-        return $this->response->created();
-    }
-
     public function recovery(Request $request)
     {
         $validator = Validator::make($request->only('email'), [
@@ -108,7 +83,7 @@ class AuthController extends Controller
 
         if($validator->fails())
             throw new ValidationHttpException($validator->errors()->all());
-        
+
         $response = Password::reset($credentials, function ($user, $password) {
             $user->password = $password;
             $user->save();
--- app/Http/api_routes.php
+++ app/Http/api_routes.php
@@ -6,7 +6,6 @@ $api->version('v1', function ($api) {
 
     // Auth
     $api->post('auth/login', 'App\Api\V1\Controllers\AuthController@login');
-    $api->post('auth/signup', 'App\Api\V1\Controllers\AuthController@signup');
     $api->post('auth/recovery', 'App\Api\V1\Controllers\AuthController@recovery');
     $api->post('auth/reset', 'App\Api\V1\Controllers\AuthController@reset');
     $api->get('auth/refresh', 'App\Api\V1\Controllers\AuthController@token');
