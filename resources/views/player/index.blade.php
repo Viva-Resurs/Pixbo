cae05a7b91d56806bedad9f1ef3bf6f5127047c1
ToxicTree <zoi009900@gmail.com>
2017-03-24 10:13:44 +0100

    Player: relative js/css

--- resources/views/player/index.blade.php
+++ resources/views/player/index.blade.php
@@ -2,20 +2,16 @@
 <html>
     <head>
         <title>Pixbo::Player</title>
-
         <link rel="stylesheet" href="css/PixboPlayer.css?date={{ date('ymdHis') }}">
-        
-        <script src="/js/PixboPlayer.js?date={{ date('ymdHis') }}"></script>
+        <script src="js/PixboPlayer.js?date={{ date('ymdHis') }}"></script>
     </head>
     <body>
         <script>
             PixboPlayer.Start({
                 Client_ADDR : "{{ $Client_ADDR }}"
-                
                 @if(isset($preview))
                     , EnableControls : true
                 @endif
-
             });
         </script>
     </body>
